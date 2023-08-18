2f40606e4aca817bda2a11737908db7ff491bf3d
s1f10210254 <s1f10210248@iniad.org>
2023-08-18 18:23:56 +0900

    a

--- server/api/controller.ts
+++ server/api/controller.ts
@@ -1,16 +1,3 @@
-// import { defineController } from './$relay';
-
-// export default defineController(() => ({
-//   get: () => ({ status: 200, body: '' }),
-// }));
-
-// import { createTask, getTasks } from '$/repository/tasksRepository';
-// import { defineController } from './$relay';
-
-// export default defineController(() => ({
-//   get: async ({ query }) => ({ status: 200, body: await getTasks(query?.limit) }),
-//   post: async ({ body }) => ({ status: 201, body: await createTask(body.label) }),
-// }));
 import { defineController } from './$relay';
 
 export default defineController(() => ({
