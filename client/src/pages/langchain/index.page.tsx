0af84b2d96e08e2eac30025765c43559ee55f501
s1f102100793 <s1f102100793@iniad.org>
2023-08-19 18:00:29 +0900

    適当にListを読み込ませてみた

--- client/src/pages/langchain/index.page.tsx
+++ client/src/pages/langchain/index.page.tsx
@@ -1,10 +1,20 @@
+import React from 'react';
 import styles from './index.module.css';
+interface ChatWindowProps {
+  messages: string[];
+}
 
-const ChatWindow = () => {
+const ChatWindow: React.FC<ChatWindowProps> = ({ messages }) => {
   return (
     <div className={styles.chatWindow}>
       <div className={styles.header}>Chat Header</div>
-      <div className={styles.messages}>Messages...</div>
+      <div className={styles.messages}>
+        {messages.map((message, index) => (
+          <div key={index} className={styles.message}>
+            {message}
+          </div>
+        ))}
+      </div>
       <div className={styles.footer}>
         <input className={styles.inputField} placeholder="Type a message..." />
       </div>
@@ -13,13 +23,22 @@ const ChatWindow = () => {
 };
 
 const LangChain = () => {
+  // 3つの異なるメッセージリスト
+  const messagesList1 = ['FXを買いました。', 'FXを売りました。', 'FXを買いました。'];
+  const messagesList2 = ['FXを売りました。', 'FXを買いました。'];
+  const messagesList3 = [
+    'FXを買いました。',
+    'FXを売りました。',
+    'FXを売りました。',
+    'FXを買いました。',
+  ];
+
   return (
     <div className={styles.container}>
-      <ChatWindow />
-      <ChatWindow />
-      <ChatWindow />
+      <ChatWindow messages={messagesList1} />
+      <ChatWindow messages={messagesList2} />
+      <ChatWindow messages={messagesList3} />
     </div>
   );
 };
-
 export default LangChain;
