b9dc9e32e21e673c8eb720737df004036b9685fe
s1f102100793 <s1f102100793@iniad.org>
2023-08-19 16:46:08 +0900

    board/controllerのエラー無くした

--- server/api/board/controller.ts
+++ server/api/board/controller.ts
@@ -3,10 +3,13 @@ import { defineController } from './$relay';
 
 export default defineController(() => ({
   get: () => ({ status: 200, body: boardUseCace.getBoard() }),
-  // post: ({ body, user }) => ({ status: 201, body: boardUseCace.clickBoard(body, user.id) }),
+  post: async ({ body, user }) => ({
+    status: 201,
+    body: await boardUseCace.clickBoard(body, user.id),
+  }),
 
-  post: async ({ body, user }) => {
-    const result = await boardUseCace.clickBoard(body, user.id);
-    return { status: 201, body: result };
-  },
+  // post: async ({ body, user }) => {
+  //   const result = await boardUseCace.clickBoard(body, user.id);
+  //   return { status: 201, body: result };
+  // },
 }));
