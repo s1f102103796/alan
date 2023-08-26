dc2b31e4ddfb0c19c5da7fe29c6cfd8e828357b7
s1f102100793 <s1f102100793@iniad.org>
2023-08-26 16:15:50 +0900

    Revert "Merge pull request #14 from s1f102100793/online-othello"
    This reverts commit 7f1ef59c40f6f7f283820c265422d2cc7a34d713, reversing
    changes made to 6ec4063dd0c91cb74094dc0abbd19d5af9d0ebe1.

--- server/api/board/controller.ts
+++ server/api/board/controller.ts
@@ -3,13 +3,10 @@ import { defineController } from './$relay';
 
 export default defineController(() => ({
   get: () => ({ status: 200, body: boardUseCace.getBoard() }),
-  post: async ({ body, user }) => ({
-    status: 201,
-    body: await boardUseCace.clickBoard(body, user.id),
-  }),
+  // post: ({ body, user }) => ({ status: 201, body: boardUseCace.clickBoard(body, user.id) }),
 
-  // post: async ({ body, user }) => {
-  //   const result = await boardUseCace.clickBoard(body, user.id);
-  //   return { status: 201, body: result };
-  // },
+  post: async ({ body, user }) => {
+    const result = await boardUseCace.clickBoard(body, user.id);
+    return { status: 201, body: result };
+  },
 }));
