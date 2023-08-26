dc9834e66cc24e365cd2f14792d293d167fecefb
s1f102100793 <s1f102100793@iniad.org>
2023-08-26 22:04:45 +0900

    仮のフロント追加

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className={styles.container}>
       <div className={styles.conversationList}>{/* ここに会話のリストを表示 */}</div>
@@ -9,9 +11,24 @@ const Home = () => {
           <div key={index} className={styles.gridItem} />
         ))}
       </div>
-      <button className={styles.buttonAskDoraemon}>教えてドラえもん</button>
+      <button
+        className={styles.buttonAskDoraemon}
+        onClick={() => setIsModalOpen(true)} // ボタンをクリックしたらモーダルを開く
+      >
+        教えてドラえもん
+      </button>
       <div className={styles.doraemonImage} />
       <div className={styles.quote}>{/* ドラえもんの名言や会話の内容をここに書く */}</div>
+      {isModalOpen && (
+        <div className={styles.overlay}>
+          <div className={styles.inputModal}>
+            <input type="text" className={styles.inputarea} placeholder="ここに質問を入力" />
+            <button className={styles.closebutton} onClick={() => setIsModalOpen(false)}>
+              閉じる
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
