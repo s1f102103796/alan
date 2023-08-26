17c2c8e18d8cb83c36a8da50926ebe2bc98e45dd
s1f102100793 <s1f102100793@iniad.org>
2023-08-26 21:51:08 +0900

    超仮のcss作成

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -1,48 +1,18 @@
-import type { TrendModel } from 'commonTypesWithClient/models';
-import { useAtom } from 'jotai';
-import { useState } from 'react';
-import { Loading } from 'src/components/Loading/Loading';
-import { BasicHeader } from 'src/pages/@components/BasicHeader/BasicHeader';
-import { apiClient } from 'src/utils/apiClient';
-import { returnNull } from 'src/utils/returnNull';
-import { userAtom } from '../atoms/user';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [user] = useAtom(userAtom);
-  const [trends, setTrends] = useState<TrendModel[]>();
-  const fetchTrends = async () => {
-    setTrends(undefined);
-
-    const res = await apiClient.trends.$get().catch(returnNull);
-
-    if (res !== null) setTrends(res);
-  };
-
-  if (!user) return <Loading visible />;
-
   return (
-    <>
-      <BasicHeader user={user} />
-      <div className={styles.title} style={{ marginTop: '160px' }}>
-        Welcome to frourio! 頑張った！
-      </div>
-
-      <div style={{ textAlign: 'center', marginTop: '40px' }}>
-        <button onClick={fetchTrends}>Get trends</button>
-      </div>
-      <ul className={styles.tasks}>
-        {trends?.map((trend, i) => (
-          <li key={i}>
-            <label>
-              <span>
-                {i + 1}位 {trend.word}
-              </span>
-            </label>
-          </li>
+    <div className={styles.container}>
+      <div className={styles.conversationList}>{/* ここに会話のリストを表示 */}</div>
+      <div className={styles.gridContainer}>
+        {[...Array(8)].map((_, index) => (
+          <div key={index} className={styles.gridItem} />
         ))}
-      </ul>
-    </>
+      </div>
+      <button className={styles.buttonAskDoraemon}>教えてドラえもん</button>
+      <div className={styles.doraemonImage} />
+      <div className={styles.quote}>{/* ドラえもんの名言や会話の内容をここに書く */}</div>
+    </div>
   );
 };
 
