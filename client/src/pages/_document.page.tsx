c853eb44ab2ea9f409c63c27dc38e65057962c01
solufa <solufa2020@gmail.com>
2023-10-09 21:40:54 +0900

    chore: remove gtag

--- client/src/pages/_document.page.tsx
+++ client/src/pages/_document.page.tsx
@@ -1,6 +1,5 @@
 import { APP_TITLE } from 'commonConstantsWithClient';
 import { Head, Html, Main, NextScript } from 'next/document';
-import { GA_ID } from 'src/utils/gtag';
 
 function Document() {
   return (
@@ -10,19 +9,6 @@ function Document() {
         <meta name="robots" content="noindex,nofollow" />
         <meta name="description" content={APP_TITLE} />
         <link rel="icon" href="favicon.png" />
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-          }}
-        />
       </Head>
       <body>
         <Main />
