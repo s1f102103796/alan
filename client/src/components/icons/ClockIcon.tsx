4987bf0c793b7953ecca4d3c9a781ba68dc6da87
s1f102100793 <s1f102100793@iniad.org>
2023-12-29 16:18:34 +0900

    fix: ClockIcon component to use setInterval instead of setTimeout

--- client/src/components/icons/ClockIcon.tsx
+++ client/src/components/icons/ClockIcon.tsx
@@ -4,20 +4,11 @@ export const ClockIcon = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
-    const now = new Date();
-    const msUntilNextMinute = (60 - now.getSeconds()) * 1000 - now.getMilliseconds();
-    const timerId = setTimeout(() => {
+    const timerId = setInterval(() => {
       setCurrentTime(new Date());
-      const intervalId = setInterval(() => {
-        setCurrentTime(new Date());
-      }, 60000);
+    }, 1000);
 
-      return () => clearInterval(intervalId);
-    }, msUntilNextMinute);
-
-    return () => {
-      clearTimeout(timerId);
-    };
+    return () => clearInterval(timerId);
   }, []);
 
   const formatTime = (date: Date) => {
