87dcbfde1424c4be564a5a7434f13cf428a94479
solufa <solufa2020@gmail.com>
2024-01-13 01:02:18 +0900

    refactor: use symlink in server/api

--- client/next.config.js
+++ client/next.config.js
@@ -7,4 +7,8 @@ module.exports = {
   transpilePackages: ['api'],
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  webpack: (config) => {
+    config.resolve.symlinks = false;
+    return config;
+  },
 };
--- client/src/utils/apiClient.ts
+++ client/src/utils/apiClient.ts
@@ -1,5 +1,5 @@
 import aspida from '@aspida/axios';
-import api from 'api/$api';
 import axios from 'axios';
+import api from '../api/$api';
 
 export const apiClient = api(aspida(axios.create({ withCredentials: true })));
